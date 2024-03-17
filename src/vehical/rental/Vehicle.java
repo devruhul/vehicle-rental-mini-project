4c0b6ff14d680b97b17a743d6bd0a0a650e1e256
Ruhul Amin <ruhulamin262001@gmail.com>
2024-03-17 23:07:21 +0000

    update the code

--- src/vehical/rental/Vehicle.java
+++ src/vehical/rental/Vehicle.java
@@ -4,8 +4,6 @@ package vehical.rental;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-
 /**
  *
  * @author ra1435
